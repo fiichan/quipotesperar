b52add4ca00f845f210c86da68e9b3f75a4a90e9
FutureCake <thomas.c.hoek@gmail.com>
2022-05-13 17:04:54 +0200

    added basic map code. updated folder structure. as result moved the image in the old img folder to its new location in data/images

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,22 @@
+const marker_icon = L.icon({
+    iconUrl: 'leaf-green.png',
+    shadowUrl: 'leaf-shadow.png',
+
+    iconSize:     [38, 95], // size of the icon
+    shadowSize:   [50, 64], // size of the shadow
+    iconAnchor:   [22, 94], // point of the icon which will correspond to marker's location
+    shadowAnchor: [4, 62],  // the same for the shadow
+    popupAnchor:  [-3, -76] // point from which the popup should open relative to the iconAnchor
+});
+
 const map = L.map('map').setView([51.505, -0.09], 13);
+
+const basemap = L.tileLayer('https://{s}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}{r}.png', {
+	attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>',
+	subdomains: 'abcd',
+	maxZoom: 20
+});
+
+basemap.addTo(map);
+
+const marker = L.marker([51.5, -0.09]).addTo(map);
