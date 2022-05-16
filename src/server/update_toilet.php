7d514a71ee9e5e95a26c3c8137ab6c8eeafa51d2
FutureCake <thomas.c.hoek@gmail.com>
2022-05-16 14:52:25 +0200

    added minimal non functional code to update toilet parameters.

--- src/server/update_toilet.php
+++ src/server/update_toilet.php
@@ -1,3 +1,24 @@
 <?php
+    include "connect_db.php";
 
+    $stmt = $pdo->query('SELECT * FROM toilets WHERE id = ?');
+    $stmt->execute([$id]);
+
+    $toilet = $stmt->fetch();
+    $toilet = sanitize_values($toilet);
+
+    $stmt = $pdo->prepare('UPDATE toilets SET /* set variables here that need to be updated */ WHERE id = ?');
+    $stmt->execute([$bonus, $id]);
+
+    function sanitize_values($user_input) {
+        throw new Exception("not implemented", 1);
+    }
+
+    function update_toilet_values($curr, $new) {
+        throw new Exception("not implemented", 1);
+    }
+
+    function build_query($values) {
+        throw new Exception("not implemented", 1);
+    }
 ?>
