aed383cc8eda843efb59b5bb9aa70ed0e01fd9ab
FutureCake <thomas.c.hoek@gmail.com>
2022-05-29 20:33:04 +0200

    renamed interaction  to update_ui. Added placeholder functions in update_ui.

---
+++ src/js/update_ui.js
@@ -0,0 +1,17 @@
+const toilet_name = document.getElementById();
+const address = document.getElementById();
+const toilet_type = document.getElementById();
+const sub_category = document.getElementById();
+const notes = document.getElementById();
+
+function update_ui(data) {
+    
+}
+
+function currently_open(time_str, curr_time) {
+    
+}
+
+function set_hours(time_str) {
+    
+}
