677835874968eba09646d2b584a214ac52b80187
FutureCake <thomas.c.hoek@gmail.com>
2022-05-29 22:57:05 +0200

    added get element by id functions. Implemented create google search address. Added additional function definitions

--- src/js/update_ui.js
+++ src/js/update_ui.js
@@ -1,11 +1,21 @@
-const toilet_name = document.getElementById();
-const address = document.getElementById();
-const toilet_type = document.getElementById();
-const sub_category = document.getElementById();
-const notes = document.getElementById();
+const base_url = "https://www.google.com/maps/place/";
+
+const toilet_name = document.getElementById("toilet-name");
+const address = document.getElementById('address');
+const address_url = document.getElementById('google_maps_url')
+// const toilet_type = document.getElementById();
+// const sub_category = document.getElementById();
+// const notes = document.getElementById();
 
 function update_ui(data) {
-    
+    toilet_name.innerHTML = data.name;
+    generate_google_maps_url(data.address);
+}
+
+function generate_google_maps_url(address_str) {
+    const google_url = base_url + address_str.replaceAll(' ', '+');
+    address.innerHTML = address_str;
+    address_url.setAttribute('href', google_url);
 }
 
 function currently_open(time_str, curr_time) {
@@ -13,5 +23,9 @@ function currently_open(time_str, curr_time) {
 }
 
 function set_hours(time_str) {
+    
+}
+
+function generate_hours_object(time_str) {
     
 }
