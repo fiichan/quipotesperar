23b97e1f891faed350572efa5abd60311ad62656
FutureCake <thomas.c.hoek@gmail.com>
2022-06-01 00:00:55 +0200

    fixed some shitty html. fixed all time related issues.

--- src/js/update_ui.js
+++ src/js/update_ui.js
@@ -8,7 +8,7 @@ const location_type = document.getElementById('location-type')
 const toilet_type = document.getElementById('toilet-type')
 const open_now = document.getElementById('toilet-open-now');
 const out_of_order = document.getElementById('toilet-out-of-order');
-const times = document.getElementsByClassName('times');
+const times = process_time_elements(document.getElementsByClassName('times'));
 const notes = document.getElementById('notes');
 const notes_content = document.getElementById('notes-content');
 
@@ -18,6 +18,16 @@ function update_ui(data) {
     set_hours(data.times);
     out_of_order.classList.toggle('is-hidden', !!!+data.out_of_order);
     set_notes(data.notes.trim());
+}
+
+function process_time_elements(els) {
+    const time_els = {};
+    for (let i = 0; i < els.length; i++) {
+        const el = els[i];
+        time_els[el.dataset.time] = el;
+    }
+
+    return time_els;
 }
 
 function set_notes(info) {
@@ -38,21 +48,21 @@ function generate_google_maps_url(address_str) {
 }
 
 function currently_open(days_hours) {
-    
+
     const now = new Date();
     const day = now.getDay() - 1;
 
     for (let i = 0; i < days_hours.length; i++) {
         const ds_hs = days_hours[i];
-    
-        if(ds_hs[0].length === 1) {
-            if(days.indexOf(ds_hs[0][0]) === day) {
-                if(check_time(now, ds_hs[1])) return true;
+
+        if (ds_hs[0].length === 1) {
+            if (days.indexOf(ds_hs[0][0]) === day) {
+                if (check_time(now, ds_hs[1])) return true;
             }
-    
+
         } else {
-            if(day >= days.indexOf(ds_hs[0][0]) && day <= days.indexOf(ds_hs[0][1])) {
-                if(check_time(now, ds_hs[1])) return true;
+            if (day >= days.indexOf(ds_hs[0][0]) && day <= days.indexOf(ds_hs[0][1])) {
+                if (check_time(now, ds_hs[1])) return true;
             }
         }
     }
@@ -65,28 +75,36 @@ function check_time(curr, hours) {
 
     for (let i = 0; i < hours.length; i++) {
         const range = hours[i];
-        if(now >= range[0] && now <= range[1]) return true;
+        if (now >= range[0] && now <= range[1]) return true;
     }
-    
+
     return false;
 }
 
 function set_hours(time_str) {
+
     const days_hours = deconstruct_time_str(time_str);
-    let is_open = (days_hours != null) ? currently_open(days_hours) : false;
-    console.log(time_str);
+    let is_open = false;
+
+    if ((days_hours != null)) {
+        is_open = currently_open(days_hours);
+        set_opening_hours_ui(days_hours);
+    } else {
+        set_opening_hours_ui(null);
+    }
+
     open_now.classList.toggle('is-hidden', !is_open);
 }
 
 function deconstruct_time_str(time_str) {
 
-    if(!time_str) return null;
+    if (!time_str) return null;
 
     time_str = time_str.toLowerCase().replaceAll(':', '.');
     const time_strs = time_str.split(',');
 
-    if (time_strs.length === 0) { 
-        return null; 
+    if (time_strs.length === 0) {
+        return null;
     } else {
         const openings_hours = [];
         time_strs.forEach(str => {
@@ -98,26 +116,26 @@ function deconstruct_time_str(time_str) {
 
 function time_str_times_array(str) {
 
-    if( str.match(/[a-zA-Z]{3}/) != null) {
+    if (str.match(/[a-zA-Z]{3}/) != null) {
 
         const days_hours = str.split(';');
         days_hours[0] = days_hours[0].split('-');
         days_hours[1] = days_hours[1].split('&');
         days_hours[1] = hours_strs_to_floats(days_hours[1]);
-        
+
         return days_hours;
 
     } else {
         return [
-            ['mon', 'sun'], 
+            ['mon', 'sun'],
             hours_strs_to_floats(str.split('&'))
         ];
     }
-    
+
 }
 
 function hours_strs_to_floats(hour_strs) {
-    
+
     for (let i = 0; i < hour_strs.length; i++) {
         const raw_hours = hour_strs[i];
         hour_strs[i] = raw_hours.split('-');
@@ -131,12 +149,49 @@ function hours_strs_to_floats(hour_strs) {
     return hour_strs;
 }
 
-function get_days(hours, start, end) {
+function set_opening_hours_ui(opening_hours) {
 
-    for (let i = days.indexOf(start); i < days.indexOf(end) + 1; i++) {
-        const day = { day: days[i], times: hours.join(' and ') };
-        open_days.push(day);
+    if(opening_hours === null) {
+        for (const day in times) {
+            const el = times[day];
+            el.innerHTML = "desconocido";
+        }
+
+        return;
     }
 
-    return open_days;
+    const checked_days = days.reduce((accumulator, value) => {
+        return {...accumulator, [value]: ''};
+    }, {});
+
+    opening_hours.forEach(time => {
+        if(time[0].length === 1) {
+            const day = time[0][0];
+            delete checked_days[day];
+            times[day].innerHTML = hours_to_string(time[1]);
+        } else {
+            for (let i = days.indexOf(time[0][0]); i < days.indexOf(time[0][1]) + 1; i++) {
+                delete checked_days[days[i]];
+                times[days[i]].innerHTML = hours_to_string(time[1]);
+            }
+        }
+    });
+
+    for (const day in checked_days) {
+        times[day].innerHTML = "cerrado";
+    }
+}
+
+function hours_to_string(hours) {
+
+    const hour_strs = [];
+    hours.forEach(hour => {
+        const formatted = [];
+        hour.forEach(mins => {
+            formatted.push(mins.toFixed(2));
+        });
+        hour_strs.push(formatted.join('-'));
+    });
+
+    return hour_strs.join('&nbsp;&nbsp;y&nbsp;&nbsp;').replaceAll('.', ':');
 }
